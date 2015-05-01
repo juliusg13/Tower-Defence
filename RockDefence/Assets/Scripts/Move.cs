b83a23f5601454321600e6ebcd9b6be06eacd8f5
Julius Gislason <juliusg13@ru.is>
2015-05-01 18:55:44 +0000

    Groupie is running now

---
+++ RockDefence/Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate (){
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+	}
+}
