fc6b5d9e91d1c70ae1525ee7ed89cf3fd006b0be
Julius Gislason <juliusg13@ru.is>
2015-05-02 16:40:13 +0000

    Fuck it tits

--- RockDefence/Assets/Scripts/Move.cs
+++ RockDefence/Assets/Scripts/Move.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Move : MonoBehaviour {
 
 	public float moveSpeed;
+	int Direction = 0; // 0=right, 1 = up, 2 = down, 4 = left.
 	// Use this for initialization
 	void Start () {
 	
@@ -15,11 +16,16 @@ public class Move : MonoBehaviour {
 	}
 
 	void FixedUpdate (){
-		//GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
-	
+
+		if (Direction == 0) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+		} else if (Direction == 2) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * 0, GetComponent<Rigidbody2D> ().velocity.y * moveSpeed);
+		}
+	//	transform.Rotate (0, 0, 10);
 		//BARA FLIPP MEÐ IF OG ELSE EIGUM SENNILEGA eKKI að GERA ÞETTA SVONA
 
-		if (GetComponent<Rigidbody2D> ().position.x < -1.9) {
+	/*	if (GetComponent<Rigidbody2D> ().position.x < -1.9) {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
 		}
 		else if (GetComponent<Rigidbody2D> ().position.y < 0.55) {
@@ -33,17 +39,27 @@ public class Move : MonoBehaviour {
 		else {
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (transform.localScale.x * 0, GetComponent<Rigidbody2D> ().velocity.y);
 
-		}
+		}*/
 	}
-	void OnCollisionEnter(Collision colis)
+	void OnTriggerEnter2D(Collider2D other){
+		float xValue;
+		xValue = GetComponent<Rigidbody2D> ().position.x - 1f;
+		Direction = 2;
+		transform.Rotate (0, 0, 90);
+	}
+
+	void OnCollisionEnter2D(Collision2D colis)
 	{
+		//colis.transform.Rotate (Vector3.right);
+		//colis.transform.Rotate (Vector3.up);
+	//	transform.Rotate (0, 0, 90);
 		//I tried .tag also but neither seemed to work
-		if (colis.gameObject.name == "Grass") {
-			Debug.Log("hit");
-			Destroy(colis.gameObject);
+		//if (colis.gameObject.name == "Grass") {
+		//	Debug.Log("hit");
+		//	Destroy(colis.gameObject);
 			//GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x,transform.localScale.y * moveSpeed);
 			//GetComponent<Rigidbody2D> ().AddForce(transform.forward * 2);
-		}
+		//}
 	}
 
 }
