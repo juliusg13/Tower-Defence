abd6872fba3c62a1466515afee26a23664c07961
binni11 <bro3@hi.is>
2015-05-03 15:56:26 +0000

    Bætti við enemy

---
+++ RockDefence/Assets/Scripts/Groupie.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Groupie : MonoBehaviour {
+
+	public GameObject groupie;
+	
+	void Start() {
+		for (int i = 0; i < 5; i++) {
+			Instantiate(groupie);
+		}
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter(Collision otherObj) {
+		if (otherObj.gameObject.tag == "Stage") {
+			Destroy(gameObject,.5f);
+		}
+	}
+}
