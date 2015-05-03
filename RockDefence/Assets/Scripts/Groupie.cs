8746393c79beac992475a31ec8068f24e4f62455
Julius Gislason <juliusg13@ru.is>
2015-05-03 17:15:34 +0000

    Multiple groupies spawning
    goteeeem

--- RockDefence/Assets/Scripts/Groupie.cs
+++ RockDefence/Assets/Scripts/Groupie.cs
@@ -6,15 +6,19 @@ public class Groupie : MonoBehaviour {
 	public GameObject groupie;
 
 	void Start() {
-		//for (int i = 0; i < 5; i++) {
-			Instantiate(groupie);
-		//} 
+		float x = 1f;
+		float y = 6f;
+		float z = -1f;
+		for (int i = 0; i < 5; i++) {
+			Instantiate(groupie, new Vector3(x, y, z), transform.rotation);
+			y += 0.9f;
+		} 
 	}
 	
 	// Update is called once per frame
-	void OnCollisionEnter2D(Collision2D otherObj) {
+	/*void OnCollisionEnter2D(Collision2D otherObj) {
 		if (otherObj.gameObject.tag == "Stage") {
 			Destroy(gameObject,.5f);
 		}
-	}
+	}*/
 }
