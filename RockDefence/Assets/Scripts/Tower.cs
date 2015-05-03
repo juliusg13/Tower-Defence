19e6624027de4fb05ec395f1703faeb45f57bb79
Skorri <skorrijuliusson@gmail.com>
2015-05-03 15:19:59 +0000

    LBa

---
+++ RockDefence/Assets/Scripts/Tower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public GameObject Note;
+	public Transform shotSpawn;
+	public float fireRate;
+	// Use this for initialization
+	public float HitRatio;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		Instantiate(Note, shotSpawn.position, shotSpawn.rotation);
+	}
+}
