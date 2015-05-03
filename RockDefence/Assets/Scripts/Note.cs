0f53f8df98f7b2e910bbdc4ce52876b51a573324
Skorri <skorrijuliusson@gmail.com>
2015-05-03 19:14:33 +0000

    Shoots notes. Í Console þarf að af haka Error Pause

---
+++ RockDefence/Assets/Scripts/Note.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D otherObj) {
+		if (otherObj.gameObject.tag == "Hurtable") {
+			//Destroy(otherObj.gameObject);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void FixedUpdate (){
+		transform.Translate(new Vector3(0,speed*Time.deltaTime,0));
+	}
+
+}
---
+++ RockDefence/Assets/Scripts/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public Transform shootingPref; //Returns an error because it is not initialized
+	public double fireRate = 0.5;
+
+	public double nextShot = 0.0; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate (){
+		if (Time.time >= nextShot) {
+			Instantiate (shootingPref, new Vector3 (transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+			nextShot = Time.time + fireRate;
+		}
+
+	}
+}
