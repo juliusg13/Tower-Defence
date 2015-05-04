7fde079fb0bbeb86e25275ab5787a731120dbe44
Skorri <skorrijuliusson@gmail.com>
2015-05-04 16:28:19 +0000

    aiming

--- RockDefence/Assets/Scripts/Note.cs
+++ RockDefence/Assets/Scripts/Note.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Note : MonoBehaviour {
 	public float speed;
+	public GameObject target;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,14 +13,16 @@ public class Note : MonoBehaviour {
 		if (other.gameObject.name == "Groupie" || other.gameObject.name == "Groupie(Clone)") {
 			Destroy (this.gameObject);
 		}
-
 	}
+
 	// Update is called once per frame
 	void Update () {
 
 	}
 	void FixedUpdate (){
-		transform.Translate(new Vector3(0,speed*Time.deltaTime,0));
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+		//transform.Translate(new Vector3(0,speed*Time.deltaTime,0));
 	}
 
 }
--- RockDefence/Assets/Scripts/Shooting.cs
+++ RockDefence/Assets/Scripts/Shooting.cs
@@ -1,35 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Shooting : MonoBehaviour {
 
-	public Transform shootingPref; //Returns an error because it is not initialized
-	public double fireRate = 0.5;
-
-	public double nextShot = 0.0; 
+	public GameObject Note; //Returns an error because it is not initialized
+	public float fireRate = 0.5f;
+	public double nextShot = 0.9;
+	public bool allowFire = true;
+	
+	public List<GameObject> ObjectsInRange = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
 	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
-
+	
 	void FixedUpdate (){
+
+		if ( allowFire && (ObjectsInRange.Count > 0)) {
+			StartCoroutine(Fire ());
+		}
 		/*if (Time.time >= nextShot) {
 			Instantiate (shootingPref, new Vector3 (transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
 			nextShot = Time.time + fireRate;
 		}*/
 
 	}
+	IEnumerator Fire()
+	{
+		Debug.Log ("Fire");
+		allowFire = false;
+		GameObject blah = (GameObject)Instantiate (Note, new Vector3 (transform.position.x , transform.position.y, transform.position.z), Quaternion.identity);
+		blah.GetComponent<Note>().target = ObjectsInRange[0];
+		yield return new WaitForSeconds (1);
+		Debug.Log ("Fire2");
+		allowFire = true;
+	}
+	
 	void OnTriggerEnter2D(Collider2D other){
-		if (Time.time >= nextShot) {
-			Instantiate (shootingPref, new Vector3 (transform.position.x , transform.position.y, transform.position.z), Quaternion.identity);
-			nextShot = Time.time + fireRate;
-		}
+
+		ObjectsInRange.Add (other.gameObject);
+		Debug.Log (ObjectsInRange[0]);
+
 		
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+		ObjectsInRange.Remove (other.gameObject);
 	}
 }
