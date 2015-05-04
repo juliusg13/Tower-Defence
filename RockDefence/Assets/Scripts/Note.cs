585d71f096dfe140981c9f767b9cc8dada4fd605
binni11 <bro3@hi.is>
2015-05-04 17:03:00 +0000

    Shooting stuff

--- RockDefence/Assets/Scripts/Note.cs
+++ RockDefence/Assets/Scripts/Note.cs
@@ -3,28 +3,33 @@ using System.Collections;
 
 public class Note : MonoBehaviour {
 	public float speed;
-
+	public GameObject target;
+	
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-
+	
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.name == "Groupie" || other.gameObject.name == "Groupie(Clone)" || 
 		    other.gameObject.name == "YoungGroupie" || other.gameObject.name == "YoungGroupie(Clone)"|| 
 		    other.gameObject.name == "OldGroupie" || other.gameObject.name == "OldGroupie(Clone)") {
 			Destroy (this.gameObject);
 		}
-
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
-
+	
 	void FixedUpdate (){
-		transform.Translate(new Vector3(0,speed*Time.deltaTime,0));
+		//while (target != null) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, step);
+			//transform.Translate(new Vector3(0,speed*Time.deltaTime,0));
+		//}
 	}
-
+	
 }
