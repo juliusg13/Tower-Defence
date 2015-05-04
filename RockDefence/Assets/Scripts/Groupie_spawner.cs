4f637fa250cf8a372bf81c6580d697e7edbbeb61
Julius Gislason <juliusg13@ru.is>
2015-05-04 17:46:06 +0000

    Fixed global start location

--- RockDefence/Assets/Scripts/Groupie_spawner.cs
+++ RockDefence/Assets/Scripts/Groupie_spawner.cs
@@ -9,15 +9,17 @@ public class Groupie_spawner : MonoBehaviour {
 	public int Groupie_count;
 	public float length_between_groupies;
 
+	public float x_coord_start;
+	public float y_coord_start;
+
 	void Start() {
-		float x = 1f;
-		float y = 6f;
+
 		float z = -1f;
 		for (int i = 0; i < Groupie_count; i++) {
-			Instantiate(groupie, new Vector3(x, y, z), transform.rotation);
-			Instantiate(YoungGroupie, new Vector3(x, y, z), transform.rotation);
-			Instantiate(OldGroupie, new Vector3(x, y, z), transform.rotation);
-			y += length_between_groupies;
+			Instantiate(groupie, new Vector3(x_coord_start, y_coord_start, z), transform.rotation);
+			Instantiate(YoungGroupie, new Vector3(x_coord_start, y_coord_start, z), transform.rotation);
+			Instantiate(OldGroupie, new Vector3(x_coord_start, y_coord_start, z), transform.rotation);
+			y_coord_start += length_between_groupies;
 		} 
 	}
 	
