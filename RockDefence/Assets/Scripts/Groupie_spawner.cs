fe71360df9d02195388ef41202a54cf8652b77af
Julius Gislason <juliusg13@ru.is>
2015-05-04 15:16:36 +0000

    Fixxing Atlis shit

--- RockDefence/Assets/Scripts/Groupie_spawner.cs
+++ RockDefence/Assets/Scripts/Groupie_spawner.cs
@@ -8,13 +8,16 @@ public class Groupie_spawner : MonoBehaviour {
 	public int Groupie_count;
 	public float length_between_groupies;
 
+	public float x_coord_start;
+	public float y_coord_start;
+
 	void Start() {
 
 		float z = -1f;
 		for (int i = 0; i < Groupie_count; i++) {
-			Instantiate(groupie, new Vector3(x, y, z), transform.rotation);
+			Instantiate(groupie, new Vector3(x_coord_start, y_coord_start, z), transform.rotation);
 			//Instantiate(YoungGroupie, new Vector3(x, y, z), transform.rotation);
-			y += length_between_groupies;
+			y_coord_start += length_between_groupies;
 		} 
 	}
 	
