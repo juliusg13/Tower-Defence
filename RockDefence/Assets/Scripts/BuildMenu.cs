d29a28c3cce2364805c71a987c91e17d46b1503d
Julius Gislason <juliusg13@ru.is>
2015-05-06 15:26:30 +0000

    Merge branch 'master' of https://github.com/juliusg13/Tower-Defence
    Conflicts:
    	RockDefence/Assets/Prefabs/Blue box.prefab.meta
    	RockDefence/Assets/Sounds.meta
    	RockDefence/Assets/Sprites/Groupie.meta
    	RockDefence/Assets/Sprites/Thumbs.db.meta
    	RockDefence/Assets/Sprites/Towers.meta
    	RockDefence/Assets/Thumbs.db.meta

--- RockDefence/Assets/Scripts/BuildMenu.cs
+++ RockDefence/Assets/Scripts/BuildMenu.cs
@@ -27,7 +27,7 @@ public class BuildMenu : MonoBehaviour {
 		if (c.isMenu == true) {
 			if((this.gameObject.name == "Orange box(Clone)") && isBuilt == false){
 				GameObject BM = GameObject.Find ("Blue box(Clone)");
-				Instantiate(speaker, new Vector3(x-0.2f, y+0.75f, 1), transform.rotation);
+				Instantiate(speaker, new Vector3(x+0.3f, y, 1), transform.rotation);
 				Destroy(this.gameObject);
 				Destroy (BM);
 				c.isMenu = false;
@@ -36,20 +36,14 @@ public class BuildMenu : MonoBehaviour {
 			}
 			else if(this.gameObject.name == "Blue box(Clone)" && isBuilt == false){
 				GameObject OM = GameObject.Find ("Orange box(Clone)");
-				Instantiate (bar, new Vector3(x-0.9f,y+0.75f,1), transform.rotation);
+				Instantiate (bar, new Vector3(x-0.3f,y,1), transform.rotation);
 				Destroy(this.gameObject);
 				Destroy (OM);
 				c.isMenu = false;
 				SourceTile.GetComponent<Tile> ().built = true;
 				//built = true;
 			}
-			else {
-				GameObject OM = GameObject.Find ("Orange box(Clone)");
-				GameObject BM = GameObject.Find ("Blue box(Clone)");
-				Destroy(OM);
-				Destroy(BM);
-				c.isMenu = false;
-			}
+
 			
 		}
 
