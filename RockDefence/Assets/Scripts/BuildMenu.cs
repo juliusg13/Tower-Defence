807aef26cfd066f242da4e22bcf4717c5e765cdc
Julius Gislason <juliusg13@ru.is>
2015-05-07 15:12:06 +0000

    Fixxed buildmenu afain

--- RockDefence/Assets/Scripts/BuildMenu.cs
+++ RockDefence/Assets/Scripts/BuildMenu.cs
@@ -2,29 +2,32 @@
 using System.Collections;
 
 public class BuildMenu : MonoBehaviour {
-
+	
 	public GameObject speaker;
 	public GameObject bar;
 	public GameObject SourceTile;
-
+	public GameObject Circle;
+	public int SpeakerPrice;
+	public int BarPrice;
 	// Use this for initialization
 	void Start () {
-	
+		SpeakerPrice = 10;
+		BarPrice = 20;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 	void OnMouseDown() {
 		GameObject cont = GameObject.Find ("Controller");
 		Controller c = cont.GetComponent<Controller> ();
-
+		
 		bool isBuilt = SourceTile.GetComponent<Tile> ().built;
-
+		
 		float x = this.transform.position.x;
 		float y = this.transform.position.y;
-
+		
 		if (c.isMenu == true) {
 			if((this.gameObject.name == "Orange box(Clone)") && isBuilt == false){
 				GameObject BM = GameObject.Find ("Blue box(Clone)");
@@ -33,7 +36,13 @@ public class BuildMenu : MonoBehaviour {
 				Destroy (BM);
 				c.isMenu = false;
 				SourceTile.GetComponent<Tile> ().built = true;
+				
+				if(GameObject.FindGameObjectWithTag("MenuChild")){
+					Destroy (GameObject.FindGameObjectWithTag("MenuChild"));
+				}
+				
 				c.BuySpeaker();
+				
 				//built = true;
 			}
 			else if(this.gameObject.name == "Blue box(Clone)" && isBuilt == false){
@@ -43,12 +52,44 @@ public class BuildMenu : MonoBehaviour {
 				Destroy (OM);
 				c.isMenu = false;
 				SourceTile.GetComponent<Tile> ().built = true;
+				if(GameObject.FindGameObjectWithTag("MenuChild")){
+					Destroy (GameObject.FindGameObjectWithTag("MenuChild"));
+				}
+				
 				c.BuyBar();
 				//built = true;
 			}
-
+			
 			
 		}
-
+		
+	}
+	
+	void OnMouseEnter() {
+		
+		float x = SourceTile.GetComponent<Tile> ().transform.position.x;
+		float y = SourceTile.GetComponent<Tile> ().transform.position.y;
+		
+		if (this.gameObject.tag == "MenuBarRange" || this.gameObject.tag == "MenuSpeakerRange") {
+			//this.GetComponentInChildren<SpriteRenderer>().color = new Color(255f, 255f, 0, 1);
+			GameObject Circle1 = (GameObject)Instantiate(Circle, new Vector3(x, y, -1), transform.rotation);
+			Circle1.GetComponent<SpriteRenderer>().enabled = true;
+		}
+		/*if (this.gameObject.name == "MenuSpeakerRange") {
+			//this.GetComponentInChildren<SpriteRenderer>().color = new Color(255f, 255f, 0, 1);
+			GameObject Circle1 = (GameObject)Instantiate(Circle, new Vector3(x, y, -1), transform.rotation);
+			Circle1.GetComponent<SpriteRenderer>().enabled = true;
+		}*/
+		
+	}
+	void OnMouseExit() {
+		if (this.gameObject.tag == "MenuBarRange" || this.gameObject.tag == "MenuSpeakerRange") {
+			//while(GameObject.FindGameObjectWithTag("MenuChild") != null){
+			GameObject Circle1 = GameObject.FindGameObjectWithTag("MenuChild");
+			
+			Destroy(Circle1);
+			//}
+		}
+		
 	}
 }
