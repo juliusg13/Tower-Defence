601b14ade0f3f1fda21c2e8d5345f76605aa9bd3
binni11 <bro3@hi.is>
2015-05-12 16:10:14 +0000

    10 level

--- RockDefence/Assets/Scripts/Groupie_Behaviour.cs
+++ RockDefence/Assets/Scripts/Groupie_Behaviour.cs
@@ -107,10 +107,10 @@ public class Groupie_Behaviour : MonoBehaviour {
 
 			if (health <= 0) {
 				if(this.gameObject.name == "YoungGroupie(Clone)") controller.IncreaseRockDollars(1);
-				if(this.gameObject.name == "Groupie(Clone)") controller.IncreaseRockDollars(2);
+				if(this.gameObject.name == "MediumGroupie(Clone)") controller.IncreaseRockDollars(2);
 				if(this.gameObject.name == "OldGroupie(Clone)") controller.IncreaseRockDollars(3);
 				Destroy (this.gameObject);
-//				controller.IncreaseRockDollars();
+				//controller.IncreaseRockDollars();
 			}
 		} else if (other.gameObject.tag == "Beer") {
 
