0be77c4ae236e7128ddd2bd6c605d462870dd9d1
Skorri <skorrijuliusson@gmail.com>
2015-05-12 16:58:48 +0000

    Mute

---
+++ RockDefence/Assets/Scripts/Mute.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mute : MonoBehaviour {
+	// Use this for initialization
+	bool sound = true;
+	public Sprite MuteSprite;
+	public Sprite UnMuteSprite;
+
+	// Update is called once per frame
+
+	void OnMouseDown() {
+		//audio.Pause();
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+		if (sound) {
+			AudioListener.volume = 0.0f;
+			//Instantiate(MuteObj,new Vector3(transform.position.x, transform.position.y, -1),Quaternion.identity);
+			sr.sprite = MuteSprite;
+			sound = false;
+			//audio.mute = false;
+		} else {
+			AudioListener.volume = 1f;
+			sr.sprite = UnMuteSprite;
+			sound = true;
+		}
+			//audio.mute = true;
+	}
+	void Update() {
+		//if (Input.GetKeyDown(KeyCode.Space))
+		
+		
+	}
+}
