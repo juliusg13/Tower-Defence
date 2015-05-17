9e95f6647740dc9d84ee22a417575c45a8c919bf
Julius Gislason <juliusg13@ru.is>
2015-05-17 17:09:02 +0000

    Objects not interactable when game lost.

--- RockDefence/Assets/Scripts/Controller.cs
+++ RockDefence/Assets/Scripts/Controller.cs
@@ -351,7 +351,9 @@ public class Controller : MonoBehaviour {
 	}
 
 	public void FreezeEnemies(){
-		StartCoroutine (StopEnemy());
+		if (GameLost == false) {
+			StartCoroutine (StopEnemy ());
+		}
 	}
 
 	public IEnumerator StopEnemy(){
