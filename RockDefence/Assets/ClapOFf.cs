a22e715311ab44e83d0898deb7ae0b5cfc65b0ae
binni11 <bro3@hi.is>
2015-05-18 19:34:08 +0000

    Clappping

---
+++ RockDefence/Assets/ClapOFf.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClapOFf : MonoBehaviour {
+	AudioSource clapSound;
+	bool clapOn;
+	// Use this for initialization
+	void Start () {
+		clapSound = GetComponent<AudioSource> ();
+		clapSound.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
